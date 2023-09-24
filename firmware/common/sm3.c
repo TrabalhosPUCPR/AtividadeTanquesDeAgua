8e188785adc33e559263c8377cd71fca93e2a77a
KnightLeo <knightleo602@gmail.com>
2023-09-24 00:08:14 -0300

    correcao valvula 2

--- firmware/common/sm3.c
+++ firmware/common/sm3.c
@@ -15,7 +15,7 @@ STATE(sm3_init) {
 }
 STATE(sm3_valve_on) {
     v2(1);
-    if((s32() && s22()) || !s21()){
+    if(s32() || !s21()){
         start_chrono(&c3, veryShortDelay());
         NEXT_STATE(sm3_valve_off);
     }
